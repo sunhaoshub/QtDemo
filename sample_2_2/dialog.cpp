2f6d02103b4b4ad160565b172b4e5c375b004bc2
cjc <19851621327@163.com>
2023-12-12 22:21:44 +0800

    first commit

---
+++ sample_2_2/dialog.cpp
@@ -0,0 +1,52 @@
+﻿#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->rBtnBlack,SIGNAL(clicked()),this,SLOT(setTextColor()));
+    QObject::connect(ui->rBtnRed,SIGNAL(clicked()),this,SLOT(setTextColor()));
+    QObject::connect(ui->rBtnBlue,SIGNAL(clicked()),this,SLOT(setTextColor()));
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_checkBoxUnderline_clicked(bool checked)
+{
+    QFont font = ui->plainTextEdit->font();
+    font.setUnderline(checked);
+    ui->plainTextEdit->setFont(font);
+}
+
+void Dialog::on_checkBoxItalic_clicked(bool checked)
+{
+    QFont font  = ui->plainTextEdit->font();
+    font.setItalic(checked);
+    ui->plainTextEdit->setFont(font);
+}
+
+void Dialog::on_checkBox_3_clicked(bool checked)
+{
+    QFont font = ui->plainTextEdit->font();
+    font.setBold(checked);
+    ui->plainTextEdit->setFont(font);
+}
+
+void Dialog::setTextColor()
+{
+    QPalette palette = ui->plainTextEdit->palette();
+    if(ui->rBtnBlack->isChecked())
+        palette.setColor(QPalette::Text,Qt::black);
+    else if(ui->rBtnRed->isChecked())
+        palette.setColor(QPalette::Text,Qt::red);
+    else if(ui->rBtnBlue->isChecked())
+        palette.setColor(QPalette::Text,Qt::blue);
+    else
+        palette.setColor(QPalette::Text,Qt::black);
+    ui->plainTextEdit->setPalette(palette);
+}
---
+++ sample_2_2/dialog.h
@@ -0,0 +1,30 @@
+﻿#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+private slots:
+    void on_checkBoxUnderline_clicked(bool checked);
+
+    void on_checkBoxItalic_clicked(bool checked);
+
+    void on_checkBox_3_clicked(bool checked);
+    //三个按钮的事件集中到一个槽函数上
+    void setTextColor();
+private:
+    Ui::Dialog *ui;
+};
+
+#endif // DIALOG_H
