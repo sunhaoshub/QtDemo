ba3833c0f5f59c868ef44ddcb3edf0d46a1f8a01
cjc <19851621327@163.com>
2023-12-13 23:10:31 +0800

    混合UI

---
+++ sample_2_4/mainwindow.cpp
@@ -0,0 +1,85 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+void MainWindow::initUI()
+{
+    fLabCurFile = new QLabel();
+    fLabCurFile->setMidLineWidth(150);
+    fLabCurFile->setText("当前文件：");
+    ui->statusBar->addWidget(fLabCurFile);
+
+    progressBar = new QProgressBar();
+    progressBar->setMinimum(5);
+    progressBar->setMaximum(50);
+    progressBar->setValue(ui->textEdit->font().pointSize());
+    ui->statusBar->addWidget(progressBar);
+
+    spinFontSize = new QSpinBox();
+    ui->mainToolBar->addWidget(new QLabel("字体大小"));
+    ui->mainToolBar->addWidget(spinFontSize);
+
+    coboFont = new QFontComboBox();
+    ui->mainToolBar->addWidget(new QLabel("字体"));
+    ui->mainToolBar->addWidget(coboFont);
+
+}
+
+void MainWindow::initSignalSLots()
+{
+    connect(spinFontSize,SIGNAL(valueChanged(int)),this,SLOT(on_spinBoxFontSize_valueChanged(int)));
+    connect(coboFont,SIGNAL(currentIndexChanged(const QString&)),this,SLOT(on_comboFont_currentIndexChanged(const QString &)));
+}
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    initUI();
+    initSignalSLots();
+    setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actFontBold_triggered(bool checked)
+{
+    QTextCharFormat fmt;
+    if(checked)
+        fmt.setFontWeight(QFont::Bold);
+    else
+        fmt.setFontWeight(QFont::Normal);
+    ui->textEdit->mergeCurrentCharFormat(fmt);
+}
+
+void MainWindow::on_textEdit_copyAvailable(bool b)
+{
+    ui->actCut->setEnabled(b);
+    ui->actCopy->setEnabled(b);
+    ui->actPaste->setEnabled(ui->textEdit->canPaste());
+}
+
+void MainWindow::on_textEdit_selectionChanged()
+{
+    QTextCharFormat fmt;
+    fmt = ui->textEdit->currentCharFormat();
+    ui->actFontBold->setChecked(fmt.font().bold());
+}
+
+void MainWindow::on_spinBoxFontSize_valueChanged(int aFontSize)
+{
+    QTextCharFormat fmt;
+    fmt.setFontPointSize(aFontSize);
+    ui->textEdit->mergeCurrentCharFormat(fmt);
+    progressBar->setValue(aFontSize);
+}
+
+void MainWindow::on_comboFont_currentIndexChanged(const QString &arg1)
+{
+    QTextCharFormat fmt;
+    fmt.setFontFamily(arg1);
+    ui->textEdit->mergeCurrentCharFormat(fmt);
+}
---
+++ sample_2_4/mainwindow.h
@@ -0,0 +1,44 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QProgressBar>
+#include<QLabel>
+#include<QSpinBox>
+#include<QFontComboBox>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    QLabel *fLabCurFile;
+    QProgressBar *progressBar;
+    QSpinBox *spinFontSize;
+    QFontComboBox *coboFont;
+
+    void initUI();
+    void initSignalSLots();
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_actFontBold_triggered(bool checked);
+
+    void on_textEdit_copyAvailable(bool b);
+
+    void on_textEdit_selectionChanged();
+
+    void on_spinBoxFontSize_valueChanged(int aFontSize);
+
+    void on_comboFont_currentIndexChanged(const QString& arg1);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
